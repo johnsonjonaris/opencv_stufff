761f8eecea89edd93cda4f17dc4eb06c86810738
Johnson Keiriz <jkeiriz@tamaggo.com>
2015-10-16 16:23:51 -0400

    discover bottle edge

--- prob2.py
+++ prob2.py
@@ -19,63 +19,65 @@ i = 0
 # f = plt.figure()
 for y in range(3):
     for x in range(len(xstops)-1):
-        sub_img = bgr_img[ystops[y*2]:ystops[y*2+1], xstops[x]:xstops[x+1],:]
-        ax = plt.subplot(3, 5, i+1)
+        sub_img = gray_img[ystops[y*2]:ystops[y*2+1], xstops[x]:xstops[x+1]]
+        ax = plt.subplot(3, 10, i+1)
         images.append(sub_img)
-        my_imshow(sub_img)
-        i += 1
-plt.show()
+        my_imshow(sub_img, cmap=cm.Greys_r)
+        plt.subplot(3, 10, i+2)
+        plt.hist(sub_img.flatten())
+        i += 2
+# plt.show()
 
 # apply edge detector to images
-
 edge_images = []
-i = 0
+mask_images = []
+seed = (50,50)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 for image in images:
-    # image[image < 45] = 0
-    # filter_image = cv2.blur(image, ksize=(5,5))
-    # edge_image = cv2.Canny(filter_image, threshold1=0, threshold2=15, apertureSize=3)
-    # edge_image = cv2.erode(edge_image, kernel=np.ones((1,1),'uint8'))
-    # edge_image = cv2.dilate(edge_image, kernel=np.ones((3,3),'uint8'))
-    # edge_image2 = cv2.erode(edge_image, kernel=np.ones((2,2),'uint8'))
+    # image[image < 55] = 0
+    filter_image = cv2.blur(image, ksize=(5,5))
+    edge_image = cv2.Canny(filter_image, threshold1=0, threshold2=60, apertureSize=3)
+    edge_image = cv2.erode(edge_image, kernel=np.ones((1,1),'uint8'))
+    edge_image = cv2.dilate(edge_image, kernel=np.ones((5,5),'uint8'))
+    edge_image = cv2.erode(edge_image, kernel=np.ones((3,3),'uint8'))
     # edge_image = edge_image - edge_image2
-    # edge_image[edge_image > 0] = 255
-    # edge_images.append(edge_image.copy())
-    i += 1
+    edge_image[edge_image > 0] = 255
+    edge_images.append(edge_image.copy())
+    # fill the holes as much as possible
+    cv2.floodFill(edge_image, mask=None, seedPoint=seed, newVal=255)
+    # find the largest contour assuming it will be the outer shape of the bottle
+    contours = cv2.findContours(edge_image, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)[0]
+    print contours
+    max_contour = 0
+    idx = 0
+    for i in range(len(contours)):
+        if len(contours[i]) > max_contour:
+            max_contour = len(contours[i])
+            idx = i
+    mask = np.zeros_like(image)
+    cv2.drawContours(mask, contours=contours, contourIdx=idx, color=255)
+    mask_images.append(mask.copy())
 
 i = 0
 f = plt.figure()
-for image in images:
-    plt.subplot(1, 2, 1)
-    my_imshow(image, cmap=cm.Greys_r)
-    plt.subplot(1, 2, 2)
-    my_imshow(edge_images[i], cmap=cm.Greys_r)
+for (mask_image,edge_image) in zip(mask_images,edge_images):
+    plt.subplot(3, 10, i+1)
+    my_imshow(edge_image, cmap=cm.Greys_r)
+    plt.subplot(3, 10, i+2)
+    my_imshow(mask_image, cmap=cm.Greys_r)
+    i += 2
+# plt.show()
+
+lines_images = []
+i = 0
+for mask_image in mask_images:
+    lines = cv2.HoughLinesP(mask_image, rho=1, theta=np.pi/180.0, threshold=10, minLineLength=0, maxLineGap=0)
+    print lines
+    mask = np.zeros_like(mask_image)
+    for line in lines:
+        cv2.drawContours(mask, contours=lines, contourIdx=-1, color=255)
+    plt.subplot(3,5,i+1)
+    my_imshow(mask, cmap=cm.Greys_r)
     i += 1
-    plt.show()
-#
-# f = plt.figure()
-# i = 0
-# for edge_image in edge_images:
-#     if i == 0:
-#         print np.unique(edge_image)
-#         plt.subplot(1,2,1)
-#         image = edge_image.copy()
-#         image[image > 0] = 1
-#         my_imshow(image, cmap=cm.Greys_r)
-#         contours = cv2.findContours(image, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)[0]
-#     print contours
-#     if i == 0:
-#         plt.subplot(1,2,2)
-#         my_imshow(image, show=True, cmap=cm.Greys_r)
-#         color = np.random.randint(0,255,[1,3])[0]
-#         img = images[i].copy()
-#         print type(contours)
-#         cv2.drawContours(img, contours=contours, contourIdx=1, color=color)
-#
-#     # plt.subplot(3, 5, i+1)
-#     # for contour in contours:
-#     #     plt.plot(contours)
-#     i += 1
-#
-#
-#
-#
+
+plt.show()
