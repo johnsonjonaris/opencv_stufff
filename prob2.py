4a6a12f7b2679f24a2b7844801770fe4ad9b4329
johnson <johnson1982@sbcglobal.net>
2015-10-20 21:25:33 -0400

    refactored code

--- prob2.py
+++ prob2.py
@@ -6,25 +6,6 @@ from utilities import *
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-def lineIntersectsSquare(line, left, top, right, bottom):
-    x0 = line[0]
-    y0 = line[1]
-    x1 = line[2]
-    y1 = line[3]
-
-    def calcY(xval):
-        if x1 == x0: return float('nan')
-        return y0 + (xval - x0)*(y1 - y0)/(x1 - x0)
-    def calcX(yval):
-        if x1 == x0: return float('nan')
-        return x0 + (yval - y0)*(y1 - y0)/(x1 - x0)
-
-    if (calcX(bottom) < right and calcX(bottom) > left): return True
-    if (calcX(top) < right and calcX(top) > left): return True
-    if (calcY(left) < top and calcY(left) > bottom): return True
-    if (calcY(right) < top and calcY(right) > bottom): return True
-    return False
-
 filename = "./prob2/bottles.bmp"
 bgr_img = cv2.imread(filename)
 (w, h, na) = bgr_img.shape
@@ -53,14 +34,15 @@ seed = (50,50)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 i = 0
 margin = 20
+labels = []
 for image in images:
     print("Processing image # %d" % (i/2+1))
     # get edges of the image
     filter_image = cv2.blur(image, ksize=(5,5))
     edge_image = cv2.Canny(filter_image, threshold1=0, threshold2=60, apertureSize=3)
-    edge_image = cv2.erode(edge_image, kernel=np.ones((1,1),'uint8'))
-    edge_image = cv2.dilate(edge_image, kernel=np.ones((5,5),'uint8'))
-    edge_image = cv2.erode(edge_image, kernel=np.ones((3,3),'uint8'))
+    edge_image = cv2.erode(edge_image, kernel=np.ones((1,1), 'uint8'))
+    edge_image = cv2.dilate(edge_image, kernel=np.ones((5,5), 'uint8'))
+    edge_image = cv2.erode(edge_image, kernel=np.ones((3,3), 'uint8'))
     edge_image[edge_image > 0] = 255
     # fill the holes as much as possible
     cv2.floodFill(edge_image, mask=None, seedPoint=seed, newVal=255)
@@ -81,7 +63,7 @@ for image in images:
     out_lines = []
     for line in lines:
         # exclude oblique lines
-        angle = AngleBetweenLines(Line(line[0]), Line([0,0,1,0]))
+        angle = Line.AngleBetweenLines(Line(line[0]), Line([0,0,1,0]))
         tol = 3.75
         test = angle < tol or abs(angle - 90) < tol or abs(angle - 180) < tol
         if test:
@@ -89,27 +71,27 @@ for image in images:
     # discover lines intersection
     nLines = len(out_lines)
     points = []
-    size = mask_image.shape
+    h, w = mask_image.shape
+    frame = Rectangle(Point(0,0), w, h)
     for j in range(nLines):
         for k in range(j+1,nLines):
-            pt = getIntersection(out_lines[j], out_lines[k])
-            test = pt is not None and np.all(np.greater(pt,0)) > 0 and \
-                   pt[0] < size[1] and pt[1] < size[0]
-            if test:
+            pt = Line.GetLinesIntersection(Line(out_lines[j]), Line(out_lines[k]))
+            if pt is not None and frame.contains(pt):
                 points.append(pt)
-    x1 = min(points[0][0], points[1][0])
-    x2 = max(points[0][0], points[1][0])
-    y2 = min(points[0][1], points[1][1])
+    x1 = min(points[0].x, points[1].x)
+    x2 = max(points[0].x, points[1].x)
+    y2 = min(points[0].y, points[1].y)
     y1 = y2 - 100
     # extract label area from image
     label = image[y1:y2,x1:x2]
+    labels.append(label)
     # analyze label area
     filter_image = cv2.blur(label, ksize=(3,3))
     edge_image = cv2.Canny(filter_image, threshold1=5, threshold2=30,
                            apertureSize=3, L2gradient=True)
-    edge_image[edge_image > 0] = 25500
+    edge_image[edge_image > 0] = 255
     # test for label existence
-    sum = np.sum(edge_image[20:-20,20:-20])
+    sum = np.sum(edge_image[20:-20, 20:-20])
     label_exist = not (sum == 0)
     correct_label = label_exist
     if label_exist:
@@ -121,16 +103,14 @@ for image in images:
         edge_image = np.zeros_like(label)
         (h, w) = edge_image.shape
         rect = Rectangle(Point(margin, margin), w-2*margin, h-2*margin)
+        frame = Rectangle(Point(0,0), w, h)
         side_lines = []
         for line in lines:
             # exclude oblique lines
-            angle = AngleBetweenLines(Line(line[0]), Line([0,0,1,0]))
+            angle = Line.AngleBetweenLines(Line(line[0]), Line([0, 0, 1, 0]))
             tol = 3.75
             test1 = angle < tol or abs(angle - 90) < tol or abs(angle - 180) < tol
-            test2 = False
-            if test1:
-                test2 = LineIntersectsRect(Line(line[0]), rect)
-            if test1 and not test2:
+            if test1 and not Line.LineIntersectsRect(Line(line[0]), rect):
                 pt1 = tuple(line[0][0:2])
                 pt2 = tuple(line[0][2:])
                 cv2.line(edge_image, pt1, pt2, color=(255,))
@@ -140,11 +120,9 @@ for image in images:
         intersection_pts = []
         for j in range(nLines):
             for k in range(j+1, nLines):
-                pt = getIntersection(side_lines[j], side_lines[k])
+                pt = Line.GetLinesIntersection(Line(side_lines[j]), Line(side_lines[k]))
                 # if the point is not within the label image, then it is incorrect
-                test = pt is not None and np.all(np.greater(pt,0)) > 0 and \
-                       pt[0] < w and pt[1] < h
-                if test:
+                if pt is not None and frame.contains(pt):
                     intersection_pts.append(pt)
         # infer corner points
         bottom_left = []
@@ -152,16 +130,19 @@ for image in images:
         top_left = []
         top_right = []
         for pt in intersection_pts:
-            if pt[0] < w/2 and pt[1] < h/2:
+            if pt.x < w/2 and pt.y < h/2:
                 bottom_left.append(pt)
-            elif pt[0] < w/2:
+            elif pt.x < w/2:
                 bottom_right.append(pt)
-            elif pt[0] > w/2 and pt[1] < h/2:
+            elif pt.x > w/2 and pt.y < h/2:
                 top_left.append(pt)
             else:
                 top_right.append(pt)
         # if we couldn't find intersection points in all four corners then the label is damaged
-        correct_label = len(bottom_left) > 0 and len(bottom_right) > 0 and len(top_left) > 0 and len(top_right) > 0
+        correct_label = len(bottom_left) > 0 and \
+                        len(bottom_right) > 0 and \
+                        len(top_left) > 0 and \
+                        len(top_right) > 0
     plt.subplot(3,10,i+1)
     my_imshow(label, cmap=cm.Greys_r)
     plt.subplot(3,10,i+2)
@@ -170,6 +151,3 @@ for image in images:
     i += 2
 
 plt.show()
-
-
-
